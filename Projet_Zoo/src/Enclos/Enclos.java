522410841d3135edf7d8751a2272c36db969a100
Nguyen <58890310+nguyenj-c@users.noreply.github.com>
2021-12-13 06:21:50 +0100

    Modification menu

--- Projet_Zoo/src/Enclos/Enclos.java
+++ Projet_Zoo/src/Enclos/Enclos.java
@@ -7,6 +7,7 @@ package Enclos;
 import java.util.*;
 
 import Animaux.Animaux;
+import Game.initGame;
 
 public class Enclos {
 	private String name;
@@ -209,34 +210,33 @@ public class Enclos {
 			System.out.println("Etat enclos : " + getActualProprete() + ", Nb animals : " + getNbAnimals());
 		}
 	}
-		public static void menu() {
-		System.out.println("\nMenu de gestion des enclos :\n" + "1 - Créer un nouvel enclos\n"
-				+ "2 - Examiner un enclos\n" + "3 - Ajouter un animal à un enclos\n"
-				+ "4 - Supprimer un animal d'un enclos\n" + "5 - Transférer les animaux d'un enclos à un autre\n"
-				+ "6 - Nettoyer un enclos\n" + "7 - Nourrir les animaux d'un enclos\n"
-				+ "8 - Consulter les informations d'un enclos\n" + "9 - Retour");
-	}
 	
 	@Override
 	public String toString() {
 		return "Enclos { name=" + name + ", supercifie=" + superficie + ", maxAnimals=" + maxAnimals + ", nbAnimals="
 				+ nbAnimals + ", actualAnimals=" + actualAnimals + " }";
 	}
-	
-	public static void menuCreateEnclosure() {
+	public static void affichageMenu() {
+		System.out.println("\nMenu de gestion des enclos :\n" + "1 - Créer un nouvel enclos\n"
+				+ "2 - Examiner un enclos\n" + "3 - Ajouter un animal à un enclos\n"
+				+ "4 - Supprimer un animal d'un enclos\n" + "5 - Transférer les animaux d'un enclos à un autre\n"
+				+ "6 - Nettoyer un enclos\n" + "7 - Nourrir les animaux d'un enclos\n"
+				+ "8 - Consulter les informations d'un enclos\n" + "9 - Retour");
+	}
+	public static void affichageMenuCreateEnclos() {
 		System.out.println("Création d'un nouvel enclos.\n" + "Quel type d'enclos voulez vous ajouter au Zoo ?\n"
 				+ "1 - Un enclos normal\n" + "2 - Une volière\n" + "3 - Un aquarium");
 	}
 
-	public static void getNameEnclosure() {
+	public static void getNameEnclos() {
 		System.out.println("Quel nom voulez vous donner à l'enclos ?");
 	}
 
-	public static void getAreaEnclosure() {
+	public static void getAreaEnclos() {
 		System.out.println("Quel est la superficie de l'enclos ?");
 	}
 
-	public static void getMaxAnimalsEnclosure() {
+	public static void getMaxAnimalsEnclos() {
 		System.out.println("Quel est le nombre maximal d'animaux pouvant être mit dans l'enclos ?");
 	}
 
