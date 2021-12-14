92a5448fbe52005428c8b02b38c15c74ff098e1d
Nguyen <58890310+nguyenj-c@users.noreply.github.com>
2021-12-14 20:45:32 +0100

    Nouveau menu

--- Projet_Zoo/src/Game/initGame.java
+++ Projet_Zoo/src/Game/initGame.java
@@ -2,13 +2,21 @@ package Game;
 
 import java.util.Scanner;
 
+import Animaux.Animaux;
+import Enclos.Enclos;
+import Game.Menu.menuAnimaux;
+import Game.Menu.menuEmploye;
+import Game.Menu.menuEnclos;
 import Zoo.Employe;
 import Zoo.Zoo;
 
 public class initGame {
-	
 
-    private boolean game = true;
+	private static boolean game = true;
+    private static final Scanner scanner = new Scanner(System.in);
+    private static Animaux animal;
+    private static Employe employe;
+    private static Zoo zoo;
     public initGame() {}
     
 	public static void startGame() {
@@ -23,12 +31,17 @@ public class initGame {
 		System.out.println("Voulez-vous vraiment quitter la partie ? (Oui ou Non)");
 		Scanner scanner = new Scanner(System.in);
 		String selected = scanner.next();
-		if (selected.equals("Oui")) {
+		if (selected.equals("Oui") || selected.equals("oui")) {
 			game = false;
 			System.out.println("Fin de la partie ! Merci d'avoir joué.");
 		}
 	}
     public boolean isGame() {return game;}
+	
+	public static Zoo getZoo() {return zoo;}
+
+    public static Employe getEmploye() {return employe;}
+	
 	public static void mainMenu() {
 		System.out.println("\nMenu principal :\n" + "1 - Gestion des animaux\n" + "2 - Gestion des enclos\n"
 				+ "3 - Informations du Zoo\n" + "4 - Règles du jeu\n" + "5 - Quitter la partie");
@@ -40,10 +53,10 @@ public class initGame {
 		int selected = scanner.nextInt();
 		switch (selected) {
 		case 1:
-			Animaux.menu();
+			menuAnimaux.menu();
 			break;
 		case 2:
-			Enclos.menu();
+			menuEnclos.menu();
 			break;
 		case 3:
 			
@@ -63,31 +76,30 @@ public class initGame {
 		startGame();
 		System.out.print("Pour commencer, nous allons créer un employé qui sera chargé de gérer et de s'occuper du Zoo.");
 		Scanner scanner = new Scanner(System.in);
-		Employe.getNameEmploye();
+		menuEmploye.getNameEmploye();
 		String nameEmploye = scanner.next();
-		Employe.getSexEmploye();
+		menuEmploye.getSexEmploye();
 		String sexe = scanner.next();
 		boolean sexe1;
-		if (sexe.equals("Homme")) {
+		if (sexe.equals("Homme") || sexe.equals("homme") || sexe.equals("H") || sexe.equals("h")) {
 			sexe1 = true;
-		} else if (sexe.equals("Femme")) {
+		} else if (sexe.equals("Femme") || sexe.equals("femme") || sexe.equals("F") || sexe.equals("f")) {
 			sexe1 = false;
 		} else {
 			throw new IllegalArgumentException("Le sexe de l'employé n'a pas été correctement renseigné.");
 		}
-		Employe.getAgeEmploye();
+		menuEmploye.getAgeEmploye();
 		int ageEmploye = scanner.nextInt();
-		Employe employe = new Employe(nameEmploye, sexe1, ageEmploye);
-		Employe.successEmploye();
+		employe = new Employe(nameEmploye, sexe1, ageEmploye);
+		menuEmploye.successEmploye();
 		System.out.print(employe + "\n");
 		Zoo.getNameZoo();
 		String nameZoo = scanner.next();
 		Zoo.getMaxEnclos();
 		int maxEnclosure = scanner.nextInt();
-		Zoo zoo = new Zoo(nameZoo, employe, maxEnclosure);
+		zoo = new Zoo(nameZoo, employe, maxEnclosure);
 
 		System.out.print(zoo);
-
 	}
 
 }
