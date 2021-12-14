0f4bdfd41953625897d815b00e191fcccb3296a0
Nguyen <58890310+nguyenj-c@users.noreply.github.com>
2021-12-14 20:35:08 +0100

    Nouvel classe menuEmploye

--- Projet_Zoo/src/Zoo/Employe.java
+++ Projet_Zoo/src/Zoo/Employe.java
@@ -144,21 +144,5 @@ public class Employe {
 	public String toString() {
 		return "Employe { name= " + name + ", sexe= " + sexe + ", age= " + age + " }";
 	}
-	
-        public static void getNameEmploye() {
-                System.out.println("Quel est le nom de votre employé ?");
-        }
-
-        public static void getSexEmploye() {
-                System.out.println("Quel est le sexe de votre employé ? (Homme ou Femme)");
-        }
-
-        public static void getAgeEmploye() {
-                System.out.println("Quel est l'age de votre employé ?");
-        }
-        public static void successEmploye() {
-                System.out.println("L'employé à été créé !\n" +
-                                   "Nous allons maintenant créer le Zoo.");
-        }
 }
 
