fada439346a39a121faf48b85d5cbde17a31e61b
Nguyen <58890310+nguyenj-c@users.noreply.github.com>
2021-12-15 12:50:14 +0100

    Menu stable

--- Projet_Zoo/src/Game/Menu/menuZoo.java
+++ Projet_Zoo/src/Game/Menu/menuZoo.java
@@ -25,43 +25,43 @@ public class menuZoo {
     private static Animaux animal;
     private static Zoo zoo = initGame.getZoo();
 
-    public void affichageMenu() {
+    public static void affichageMenu() {
         System.out.println("\nMenu d'informations du Zoo :\n" +
                 "1 - Liste des enclos\n" +
                 "2 - Nombre d'animaux dans le Zoo\n" +
                 "3 - Animaux par enclos\n" +
                 "4 - Informations du Zoo\n" +
-                "5 - Informations de l'employé\n" +
+                "5 - Informations de l'employÃ©\n" +
                 "6 - Retour");
     }
 
-    public void listEnclos(List<String> enclosures) {
+    public static void listEnclos(List<String> enclosures) {
         System.out.println("Liste des enclos du Zoo :\n" +
                 enclosures);
     }
 
-    public void numberAnimal(int number) {
-        System.out.println("Nombre d'animaux présent dans les cages :\n" +
+    public static void numberAnimal(int number) {
+        System.out.println("Nombre d'animaux prÃ©sent dans les cages :\n" +
                 number);
     }
 
-    public void animalsByEnclos(String sortedMisOfThings) {
-        System.out.println("Enclos et leurs animaux triés par nom :\n" +
+    public static void animalsByEnclos(String sortedMisOfThings) {
+        System.out.println("Enclos et leurs animaux triÃ©s par nom :\n" +
                 sortedMisOfThings);
     }
 
-    public void informationsZoo(String zoo) {
+    public static void informationsZoo(String zoo) {
         System.out.println("Informations du zoo :\n" +
                 zoo);
     }
 
-    public void informationsEmployee(String employee) {
-        System.out.println("Informations de l'employé :\n" +
+    public static void informationsEmployee(String employee) {
+        System.out.println("Informations de l'employÃ© :\n" +
                 employee);
     }
 
     
-    public void menu() {
+    public static void menu() {
         affichageMenu();
         int selected = scanner.nextInt();
         switch (selected) {
@@ -72,38 +72,38 @@ public class menuZoo {
                 numberAnimalZoo();
                 break;
             case 3:
-    animalsByEnclos();
+            	animalsByEnclos();
                 break;
             case 4:
-    informationsZoo();
+            	informationsZoo();
                 break;
             case 5:
-    informationsEmployee();
+            	informationsEmploye();
                 break;
             case 6:
                   initGame.menu();
                 break;
-    default:
-            throw new IllegalArgumentException("Sélection en dehors du menu.");
-}
+		    default:
+		            throw new IllegalArgumentException("SÃ©lection en dehors du menu.");
+		}
     }
 
-    public void listEnclos() {
+    public static void listEnclos() {
         List<Enclos> enclos = zoo.getAllEnclos();
         listEnclos(enclos.stream().map(Enclos::toString).collect(Collectors.toList()));
     }
 
-    public void numberAnimalZoo() {
+    public static void numberAnimalZoo() {
         int number = zoo.numberAnimalsZoo();
         numberAnimal(number);
     }
 
-    public void animalsByEnclos() {
+    public static void animalsByEnclos() {
         SortedSet<Enclos> enclosByName = new TreeSet<>(zoo.getAllEnclos());
         StringBuilder sortedOutput = new StringBuilder();
         for (Enclos enclos : enclosByName) {
             sortedOutput.append(enclos.getName()).append(":\n");
-            SortedSet<Animaux> animals = new TreeSet<>(enclos.getActualAnimalsSize());
+            SortedSet<Animaux> animals = new TreeSet<>(enclos.getActualAnimals());
             for (Animaux animal : animals) {
                 sortedOutput.append("\t").append(animal.toString()).append("\n");
             }
@@ -111,13 +111,11 @@ public class menuZoo {
         animalsByEnclos(sortedOutput.toString());
     }
 
-    public void informationsZoo() {
+    public static void informationsZoo() {
     	informationsZoo(zoo.toString());
     }
 
-    public void informationsEmployee() {
+    public static void informationsEmploye() {
         informationsEmployee(employe.toString());
     }
-}
-
 }
